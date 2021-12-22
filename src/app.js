009e149cd890c259d96a75f0dd17bcb9f37ea73e
luciaPardo <variosdemas@gmail.com>
2021-12-22 20:38:32 -0300

    saco error de port

--- src/app.js
+++ src/app.js
@@ -14,8 +14,7 @@ const PORT = process.env.PORT || 3000;
 // Run the server!
 const start = async () => {
   try {
-    fastify.listen(PORT, "0.0.0.0");
-    fastify.log.info(`server listening on ${fastify.server.address().port}`);
+    fastify.listen(PORT, "0.0.0.0").then(r => fastify.log.info(`server listening on ${r}`));
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
